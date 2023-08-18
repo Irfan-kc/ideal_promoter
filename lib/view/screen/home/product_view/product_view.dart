cb9075d98bcb2fa8e07c3c07e51e832e2d9101dc
HavasAhammed <havasahammedt33@gmail.com>
2023-08-18 18:42:20 +0530

    copy url row fixed in product view screen

--- lib/view/screen/home/product_view/product_view.dart
+++ lib/view/screen/home/product_view/product_view.dart
@@ -52,172 +52,178 @@ class _ProductViewState extends State<ProductView> {
             ? loader()
             : productProvider.singleProduct == null
                 ? const Center(child: Text("FAILED TO LOAD DATA!"))
-                : SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        CarouselView(
-                          images: (productProvider.singleProduct!.images ?? []),
-                        ),
-                        // const KHeight(24),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 24),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              productProvider.isLoading
-                                  ? const CustomShimmer(
-                                      width: double.infinity,
-                                      height: 20,
-                                      radius: 6,
-                                    )
-                                  : Text(
-                                      productProvider.singleProduct!
-                                                      .primaryLang!.name !=
-                                                  null &&
-                                              productProvider.singleProduct!
-                                                      .primaryLang!.name !=
-                                                  ''
-                                          ? productProvider
-                                              .singleProduct!.primaryLang!.name!
-                                          : "NA",
-                                      style: AppTextStyle.body1Text,
-                                    ),
-                              productProvider.isLoading
-                                  ? const Padding(
-                                      padding: EdgeInsets.only(top: 8.0),
-                                      child: CustomShimmer(
+                : Column(
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          CarouselView(
+                            images: (productProvider.singleProduct!.images ?? []),
+                          ),
+                          // const KHeight(24),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 24),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                productProvider.isLoading
+                                    ? const CustomShimmer(
                                         width: double.infinity,
-                                        height: 18,
+                                        height: 20,
                                         radius: 6,
+                                      )
+                                    : Text(
+                                        productProvider.singleProduct!
+                                                        .primaryLang!.name !=
+                                                    null &&
+                                                productProvider.singleProduct!
+                                                        .primaryLang!.name !=
+                                                    ''
+                                            ? productProvider
+                                                .singleProduct!.primaryLang!.name!
+                                            : "NA",
+                                        style: AppTextStyle.body1Text,
                                       ),
-                                    )
-                                  : RichText(
-                                      text: TextSpan(
-                                        style: AppTextStyle.buttonText.apply(
-                                          color: AppColors.black,
+                                productProvider.isLoading
+                                    ? const Padding(
+                                        padding: EdgeInsets.only(top: 8.0),
+                                        child: CustomShimmer(
+                                          width: double.infinity,
+                                          height: 18,
+                                          radius: 6,
                                         ),
-                                        children: [
-                                          TextSpan(
-                                              text: productProvider
-                                                          .singleProduct!
-                                                          .price !=
-                                                      null
-                                                  ? '₹${productProvider.singleProduct!.price}'
-                                                  : '₹0',
-                                              style: const TextStyle(
-                                                  color: Color(0xFF9C9C9C),
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.w500,
-                                                  decoration: TextDecoration
-                                                      .lineThrough,
-                                                  decorationThickness: 1.5)),
-                                          const WidgetSpan(child: KWidth(10)),
-                                          TextSpan(
-                                              text: productProvider
-                                                          .singleProduct!
-                                                          .offerPrice !=
-                                                      null
-                                                  ? '₹${productProvider.singleProduct!.offerPrice}'
-                                                  : '₹0'),
-                                          const WidgetSpan(child: KWidth(10)),
-                                          TextSpan(
-                                            text: productProvider.singleProduct!
-                                                            .offerPrice !=
-                                                        null ||
-                                                    productProvider
+                                      )
+                                    : RichText(
+                                        text: TextSpan(
+                                          style: AppTextStyle.buttonText.apply(
+                                            color: AppColors.black,
+                                          ),
+                                          children: [
+                                            TextSpan(
+                                                text: productProvider
+                                                            .singleProduct!
+                                                            .price !=
+                                                        null
+                                                    ? '₹${productProvider.singleProduct!.price}'
+                                                    : '₹0',
+                                                style: const TextStyle(
+                                                    color: Color(0xFF9C9C9C),
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.w500,
+                                                    decoration: TextDecoration
+                                                        .lineThrough,
+                                                    decorationThickness: 1.5)),
+                                            const WidgetSpan(child: KWidth(10)),
+                                            TextSpan(
+                                                text: productProvider
                                                             .singleProduct!
                                                             .offerPrice !=
-                                                        0
-                                                ? "${calculateDiscount(productProvider.singleProduct!.price!, productProvider.singleProduct!.offerPrice!).toStringAsFixed(0)}% Off"
-                                                : "",
-                                            style: const TextStyle(
-                                              color: AppColors.green,
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.w500,
+                                                        null
+                                                    ? '₹${productProvider.singleProduct!.offerPrice}'
+                                                    : '₹0'),
+                                            const WidgetSpan(child: KWidth(10)),
+                                            TextSpan(
+                                              text: productProvider.singleProduct!
+                                                              .offerPrice !=
+                                                          null ||
+                                                      productProvider
+                                                              .singleProduct!
+                                                              .offerPrice !=
+                                                          0
+                                                  ? "${calculateDiscount(productProvider.singleProduct!.price!, productProvider.singleProduct!.offerPrice!).toStringAsFixed(0)}% Off"
+                                                  : "",
+                                              style: const TextStyle(
+                                                color: AppColors.green,
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.w500,
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                              const KHeight(8),
-                              productProvider.isLoading
-                                  ? const CustomShimmer(
-                                      width: double.infinity,
-                                      height: 200,
-                                      radius: 8,
-                                    )
-                                  : productProvider.singleProduct!.primaryLang!
-                                                  .description !=
-                                              null &&
-                                          productProvider.singleProduct!
-                                                  .primaryLang!.description !=
-                                              ''
-                                      ? Html(
-                                          data: productProvider.singleProduct!
-                                              .primaryLang!.description!,
-                                        )
-                                      : const Text(
-                                          "NA",
-                                          style: AppTextStyle.text,
-                                        ),
-                              const KHeight(16),
-                              Row(
-                                children: [
-                                  CustIconButton(
-                                    flex: 3,
-                                    icon: const Icon(
-                                      Icons.copy_rounded,
-                                      color: Colors.white,
-                                      size: 14,
-                                    ),
-                                    onTap: () {
-                                      var removeSpace = productProvider
-                                          .singleProduct!.primaryLang!.name!
-                                          .replaceAll(" ", "-");
-                                      var text =
-                                          "https://idealeshope.com/products/$removeSpace?ref_id=${profileProvider.profileData!.refId}";
-                                      Clipboard.setData(
-                                        ClipboardData(
-                                          text: text,
-                                        ),
-                                      ).then((_) {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(const SnackBar(
-                                                content: Text(
-                                                    "Copied to clipboard")));
-                                      });
-                                    },
-                                    text: 'Copy Url',
-                                  ),
-                                  const KWidth(10),
-                                  CustIconButton(
-                                    flex: 5,
-                                    icon: SvgPicture.asset(
-                                      'assets/icons/content_copy.svg',
-                                      height: 14,
-                                      width: 14,
-                                    ),
-                                    onTap: () {
-                                      whatsAppProvider.getItOnWhatsApp(
-                                          context,
-                                          productProvider.singleProduct!.id ??
-                                              "");
-                                    },
-                                    text: 'Get it on Whatsapp',
-                                    color: AppColors.green,
-                                  )
-                                ],
-                              ),
-                            ],
+                                const KHeight(8),
+                                productProvider.isLoading
+                                    ? const CustomShimmer(
+                                        width: double.infinity,
+                                        height: 200,
+                                        radius: 8,
+                                      )
+                                    : productProvider.singleProduct!.primaryLang!
+                                                    .description !=
+                                                null &&
+                                            productProvider.singleProduct!
+                                                    .primaryLang!.description !=
+                                                ''
+                                        ? Html(
+                                            data: productProvider.singleProduct!
+                                                .primaryLang!.description!,
+                                          )
+                                        : const Text(
+                                            "NA",
+                                            style: AppTextStyle.text,
+                                          ),
+                                const KHeight(16),
+                                
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                              children: [
+                                CustIconButton(
+                                  flex: 3,
+                                  icon: const Icon(
+                                    Icons.copy_rounded,
+                                    color: Colors.white,
+                                    size: 14,
+                                  ),
+                                  onTap: () {
+                                    var removeSpace = productProvider
+                                        .singleProduct!.primaryLang!.name!
+                                        .replaceAll(" ", "-");
+                                    var text =
+                                        "https://idealeshope.com/products/$removeSpace?ref_id=${profileProvider.profileData!.refId}";
+                                    Clipboard.setData(
+                                      ClipboardData(
+                                        text: text,
+                                      ),
+                                    ).then((_) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(const SnackBar(
+                                              content: Text(
+                                                  "Copied to clipboard")));
+                                    });
+                                  },
+                                  text: 'Copy Url',
+                                ),
+                                const KWidth(10),
+                                CustIconButton(
+                                  flex: 5,
+                                  icon: SvgPicture.asset(
+                                    'assets/icons/content_copy.svg',
+                                    height: 14,
+                                    width: 14,
+                                  ),
+                                  onTap: () {
+                                    whatsAppProvider.getItOnWhatsApp(
+                                        context,
+                                        productProvider.singleProduct!.id ??
+                                            "");
+                                  },
+                                  text: 'Get it on Whatsapp',
+                                  color: AppColors.green,
+                                )
+                              ],
+                            ),
+                    ),
+                  ],
+                ),
       );
     });
   }
