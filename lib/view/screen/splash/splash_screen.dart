6b3fb463909efacd95cf23a08f3aff1292e4ecd2
Irfan-Kc <irfankc8@gmail.com>
2023-08-18 11:23:14 +0530

    splash screen

--- lib/view/screen/splash/splash_screen.dart
+++ lib/view/screen/splash/splash_screen.dart
@@ -6,11 +6,16 @@ import 'package:ideal_promoter/view/screen/home/home.dart';
 import 'package:ideal_promoter/view/screen/login/login_screen.dart';
 import 'package:provider/provider.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
     Timer(const Duration(seconds: 5), () {
       if (Provider.of<AuthProvider>(context, listen: false).isSigned == true) {
         Navigator.pushReplacement(
@@ -20,6 +25,11 @@ class SplashScreen extends StatelessWidget {
             context, MaterialPageRoute(builder: (_) => const LoginPage()));
       }
     });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return const Scaffold(
       body: Padding(
         padding: EdgeInsets.all(20),
